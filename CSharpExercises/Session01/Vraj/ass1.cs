c9e739bbf484ab1c7827d0e0abcba88e1221287e
unknown <vrajthaker10@gmail.com>
2022-06-04 20:57:50 +0530

    myfirstcommit

---
+++ CSharpExercises/Session01/Vraj/ass1.cs
@@ -0,0 +1,8 @@
+﻿string name = "i20";
+string company = "hyundai";
+
+
+System.Console.WriteLine( "A car named" + name +"made by the company named" + company + "runs at the maximum speed of 120 km/hr." +
+    " It has got 4 wheels. Mileage given by the car is 12.5 kms/litre. System asks the user to input the number of\n Kilometres" +
+    " that user wants to drive and collects the information. Based on what user entered and current mileage, system calculates the" +
+    " number of litres (Fuel) required to make the journey");
---
+++ CSharpExercises/Session01/Vraj/ass1b.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SkillHouse // name is an identifier
+{
+    class CSharpLearning    //CSharpLearning is an identifier
+    {
+
+        // Main Method
+        static public void Main()
+        {
+
+            int a = 10;// a is an identifier
+            int b = 39;//b is an identifier
+            int c;  //c is an identifier
+
+            // simple addition
+            c = a + b;
+            System.Console.WriteLine("The sum of two number is: {0}", c);
